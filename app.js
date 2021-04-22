c2af93b8e018a8fd2fee8145106af5cc28356176
foushware <mr.fouad992@gmail.com>
2021-04-22 21:12:25 +0200

    ✅ ❌ Verify token

--- app.js
+++ app.js
@@ -4,6 +4,40 @@ const jwt = require('jsonwebtoken');
 const app = express();
 
 
+
+
+
+
+// Verify Token  ✅ ❌
+const verifyToken = (req, res, next) => {
+    // Get auth header value [token]
+
+    const bearHeader = req.headers['authorization'];
+    //Check if th bearer is undefined 
+    if (typeof bearHeader !== 'undefined') {
+
+        // Format of token
+        // Authorization: Bearer <access_token>
+
+        //split on the space 
+        const bearer = bearHeader.split(' ');
+        // Get the token from array 
+        const bearToken = bearer[1];
+        //Set the token
+        req.token = bearToken;
+        // Next middleware
+        next();
+
+    } else {
+        // Forbidden 
+        res.sendStatus(403); // undefined
+    }
+
+}
+
+// ...........  Routes 🚦 🚥   ................
+
+
 app.get('/api', (req, res) => {
     res.json({
         message: "Welcome to the API"
@@ -11,7 +45,7 @@ app.get('/api', (req, res) => {
 });
 
 //create the route to protect 
-app.post('/api/posts', (req, res) => {
+app.post('/api/posts', verifyToken, (req, res) => {
     res.json({
         message: 'Post created...'
     });
@@ -32,6 +66,10 @@ app.post('/api/login', (req, res) => {
         });
     });
 });
+
+
+
+
 
 
 const PORT = process.env.PORT || 5000;
